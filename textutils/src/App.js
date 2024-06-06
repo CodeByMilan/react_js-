0d05480b7307a92e1e310a7685730eec99f532c8
CodeByMilan <milanacharya1614@gmail.com>
2024-06-06 08:52:18 +0545

    props and propstypes

---
+++ textutils/src/App.js
@@ -0,0 +1,13 @@
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return(
+    //here the default value will be assigned to the props 
+    //<Navbar/>
+    <Navbar title="TextUtils"  about="About TextUtils"/>
+
+  ) 
+}
+
+export default App;
