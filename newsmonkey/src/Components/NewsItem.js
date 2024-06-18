f7de790e9948fbc26dbe394d6d91626a86cf50c4
CodeByMilan <milanacharya1614@gmail.com>
2024-06-18 16:15:39 +0545

    fetch api added

--- newsmonkey/src/Components/NewsItem.js
+++ newsmonkey/src/Components/NewsItem.js
@@ -9,8 +9,8 @@ export class NewsItem extends Component {
           <div className="card" style={{width: "18rem"}}>
          <img src={imgUrl} className="card-img-top" alt="..."/>
              <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
-                 <p className="card-text">{description}...</p>
+              <h5 className="card-title">{title}</h5>
+                 <p className="card-text">{description}</p>
                      <a href={newsurl}target="blank"  className="btn btn-sm btn-primary">Read more</a>
   </div>
 </div>
